f1d42e77252c65c1bf96c3aee07bd0867e233efb
Maurício Borges Note <mauriciohbcorrea@hotmail.com>
2023-10-04 18:24:31 -0300

    'add wiki part3'

--- pull-server.sh
+++ pull-server.sh
@@ -6,7 +6,7 @@ read message
 echo "Mensagem do commit: "
 git commit -am "$message"
 git push
-ssh dominioparateste.com \
+ssh 35.198.35.159 \
   'git -C /home/mauriciohbcorrea/api ' \
   'pull origin master && ' \
   'pm2 restart api && systemctl restart nginx'
